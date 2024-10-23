b11bfafa345e141ed0e0611c690cf33eb1868082
Tav-18 <tavobox3000@gmail.com>
2024-10-22 21:24:11 -0600

    plantilla agregada

--- api/login/login_views.py
+++ api/login/login_views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def login_views(request):
-    template_name = "login.html"
+    template_name = "index.html"
     
     return render(request, template_name)
